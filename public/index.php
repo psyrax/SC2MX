c355613feb864ad5b29956ea76b0af7840fa4747
Psyrax <psyrax@opiumgarden.org>
2012-02-14 02:15:09 -0600

    Prototyping

---
+++ public/index.php
@@ -0,0 +1,120 @@
+<!doctype html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		
+		<title>SC2MX</title>
+		<link rel="stylesheet" type="text/css" href="/css/bootstrap.css" />
+		<link rel="stylesheet" type="text/css" href="/css/style.css" />
+		
+	</head> 
+	<body>
+	
+	<header>
+		<div class="navbar">
+			<div class="navbar-inner">
+    			<div class="container">
+      				<a class="brand" href="#">
+  						SC2MX
+					</a>
+					<ul class="nav">
+						<li class="dropdown">
+							<a href="#"
+							      class="dropdown-toggle"
+							      data-toggle="dropdown">
+							      Streams Estelares
+							      <b class="caret"></b>
+							</a>
+							<ul class="dropdown-menu">
+							 	<li>Offline	horusstv<li>
+								<li>Offline	fenixcoaching<li>
+								<li>Offline	rommeltj<li>
+								<li>Offline	jimrsng<li>
+								<li>Offline	famousc2<li>
+								<li>Offline	zafhir<li>
+								<li>Offline	beefchief3<li>
+								<li>Offline	lowcloud1<li>
+								<li>Offline	xesk1e<li>
+								<li>Offline	day9tv<li>
+								<li>Offline	zapo_colorado<li>
+								<li>Offline	xgsrevenge<li>
+								<li>Offline	angryzergc<li>
+							</ul>
+						</li>
+						<li><a href="#">Foros</a></li>
+						<li><a href="#">Nexus</a></li>
+						<li><a href="#">LMS</a></li>
+						<li><a href="#">Facebook</a></li>
+					</ul>
+    			</div>
+  			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+			<div class="span12">
+				<h2>Fancy ad</h2>
+				<img src="http://placehold.it/940x50" />
+					<div class="row">
+			  			<div class="span8">
+			  				<div class="row">
+			  					<div class="span6">
+			  						<h1>Video principal</h1>
+			  						<img src="http://placehold.it/460x400" />
+			  					</div>
+			  					<div class="span2 otros">
+			  						<h3>Otros videos</h3>
+			  						<img src="http://placehold.it/140x100" />
+			  						<img src="http://placehold.it/140x100" />
+			  						<img src="http://placehold.it/140x100" />
+			  						<img src="http://placehold.it/140x100" />
+			  					</div>
+			  				</div>
+			  				<h3>Noticias</h3>
+			  				<hr />
+			  				<div class="row">
+			  				<?php for ($i = 1; $i <= 20; $i++) { ?>
+    							<div class="span2 anteriores">
+    								<h4>Noticia</h4>
+    								<img src="http://placehold.it/140x100" />
+    								<a href="#">Ver mas</a>
+    							</div>
+							<?php }; ?>
+							</div>
+			  			</div>
+			  			<div class="span4">
+			  				<h3>Proximos eventos</h3>
+			  				<hr />
+			  				<ul>
+			  					<li>
+			  						<span class="label label-info">Fecha y hora</span>
+			  						<img src="http://placehold.it/275x50" />
+			  						<a href="btn btn-small btn-primary">Enviame un recordatorio</a>
+			  					</li>
+			  					<li>
+			  						<span class="label label-info">Fecha y hora</span>
+			  						<img src="http://placehold.it/275x50" />
+			  						<a href="btn btn-small btn-primary">Enviame un recordatorio</a>
+			  					</li>
+			  								  					<li>
+			  						<span class="label label-info">Fecha y hora</span>
+			  						<img src="http://placehold.it/275x50" />
+			  						<a href="btn btn-small btn-primary">Enviame un recordatorio</a>
+			  					</li>
+			  								  					<li>
+			  						<span class="label label-info">Fecha y hora</span>
+			  						<img src="http://placehold.it/275x50" />
+			  						<a href="btn btn-small btn-primary">Enviame un recordatorio</a>
+			  					</li>
+			  				</ul>
+			  				<h3>Patrocinios</h3>
+			  				<hr />
+			  				<img src="http://placehold.it/300x700" />
+			  			</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+		<script src="/js/bootstrap.js"></script>
+	</body>
+</html>
+		
