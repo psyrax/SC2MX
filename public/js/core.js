bebcc9a330cc952abbc0261ef56507aca53cb72c
Eduardo Reveles <ereveles@tequiladigital.com.mx>
2012-02-17 14:33:49 -0600

    Ahora si detecta los streams en linea... ahora a ver como presentarlos..

--- public/js/core.js
+++ public/js/core.js
@@ -5,13 +5,19 @@
     {
         $('#stream-list li').each(function(i, el) {
             streamers.push($(el).html());
-        });
-
-        $.getJSON('http://api.justin.tv/api/stream/list.json?channel=' + streamers.join(',') + '&callback=?', function(data) {
-            if(data.length == 0)) {
-                return;
+            if (!$(el).attr('rel')) {
+                $(el).attr('rel', $(el).html());
             }
         });
+
+        $.getJSON('http://api.justin.tv/api/stream/list.json?jsonp=?', {channel: streamers.join(',')}, function(data) {
+            $.each(data, function(i, item) {
+                $('#stream-list li[rel=' + item.channel.login + ']').css({color: '#0f0'});
+
+            });
+        });
+
+        setTimeout(checkStreams, 60000);
     }
 
     checkStreams();
