70338868d8f787757fcae29b2a0aa970dd23e989
Psyrax <psyrax@opiumgarden.org>
2012-02-18 12:04:55 -0600

    RSS Reading

--- public/index.php
+++ public/index.php
@@ -1,3 +1,17 @@
+<?php 
+//Define feed URL
+$feed_url = 'http://www.sc2mx.com/forums/external.php?do=rss&type=newcontent&days=120&count=18';
+//Get content of the URL
+$handler = curl_init($feed_url);
+curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($handler, CURLOPT_HEADER, 0);
+$response = curl_exec ($handler);
+curl_close($handler);
+
+//Prase feeds into class
+$feeds = new SimpleXmlElement($response, LIBXML_NOCDATA);
+?>
+
 <!doctype html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -54,11 +68,11 @@
 				<h2>Fancy ad</h2>
 				<img src="http://placehold.it/940x50" />
 					<div class="row">
-			  			<div class="span8">
+			  			<div class="span9">
 			  				<div class="row">
-			  					<div class="span6">
-			  						<h1>Video principal</h1>
-			  						<img src="http://placehold.it/460x400" />
+			  					<div class="span7">
+			  						<h1>Destacado principal</h1>
+			  						<img src="http://placehold.it/540x400" />
 			  					</div>
 			  					<div class="span2 otros">
 			  						<h3>Otros videos</h3>
@@ -71,43 +85,45 @@
 			  				<h3>Noticias</h3>
 			  				<hr />
 			  				<div class="row">
-			  				<?php for ($i = 1; $i <= 20; $i++) { ?>
-    							<div class="span2 anteriores">
-    								<h4>Noticia</h4>
-    								<img src="http://placehold.it/140x100" />
-    								<a href="#">Ver mas</a>
+			  				<?php 
+			  				//Output feeds
+							foreach($feeds->channel->item as $feed) {?>
+    							<div class="span3 anteriores">
+    								<h4 class="n-title"><?= $feed->title; ?></h4>
+    								<img src="http://placehold.it/220x150" /><br />
+    								<a href="<?= $feed->link ?>">Ver mas</a>
     							</div>
 							<?php }; ?>
 							</div>
 			  			</div>
-			  			<div class="span4">
+			  			<div class="span3">
 			  				<h3>Proximos eventos</h3>
 			  				<hr />
-			  				<ul>
+			  				<ul>;
 			  					<li>
 			  						<span class="label label-info">Fecha y hora</span>
-			  						<img src="http://placehold.it/275x50" />
+			  						<img src="http://placehold.it/195x50" />
 			  						<a href="btn btn-small btn-primary">Enviame un recordatorio</a>
 			  					</li>
 			  					<li>
 			  						<span class="label label-info">Fecha y hora</span>
-			  						<img src="http://placehold.it/275x50" />
+			  						<img src="http://placehold.it/195x50" />
 			  						<a href="btn btn-small btn-primary">Enviame un recordatorio</a>
 			  					</li>
 			  								  					<li>
 			  						<span class="label label-info">Fecha y hora</span>
-			  						<img src="http://placehold.it/275x50" />
+			  						<img src="http://placehold.it/195x50" />
 			  						<a href="btn btn-small btn-primary">Enviame un recordatorio</a>
 			  					</li>
 			  								  					<li>
 			  						<span class="label label-info">Fecha y hora</span>
-			  						<img src="http://placehold.it/275x50" />
+			  						<img src="http://placehold.it/195x50" />
 			  						<a href="btn btn-small btn-primary">Enviame un recordatorio</a>
 			  					</li>
 			  				</ul>
 			  				<h3>Patrocinios</h3>
 			  				<hr />
-			  				<img src="http://placehold.it/300x700" />
+			  				<img src="http://placehold.it/220x700" />
 			  			</div>
 					</div>
 				</div>
