4e627d0ddc8cbcfa1e0fcac953482edb0b2feb61
Psyrax <psyrax@opiumgarden.org>
2012-02-18 17:04:15 -0600

    Calendar

--- public/index.php
+++ public/index.php
@@ -7,9 +7,18 @@ curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($handler, CURLOPT_HEADER, 0);
 $response = curl_exec ($handler);
 curl_close($handler);
-
 //Prase feeds into class
 $feeds = new SimpleXmlElement($response, LIBXML_NOCDATA);
+
+$calendar="https://www.google.com/calendar/feeds/fi6qgmpb0669gfecm97albjufk%40group.calendar.google.com/public/basic?singleevents=true&orderby=starttime&max-results=5";
+$calendarcurl = curl_init($calendar);
+curl_setopt($calendarcurl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($calendarcurl, CURLOPT_HEADER, 0);
+$calendarxml = curl_exec ($calendarcurl);
+curl_close($calendarcurl);
+//Prase feeds into class
+$events = new SimpleXmlElement($calendarxml, LIBXML_NOCDATA);
+//print_r($events);
 ?>
 
 <!doctype html>
@@ -109,28 +118,20 @@ $feeds = new SimpleXmlElement($response, LIBXML_NOCDATA);
 			  			</div>
 			  			<div class="span3">
 			  				<h3>Proximos eventos</h3>
-			  				<hr />
 			  				<ul>
-			  					<li>
-			  						<span class="label label-info">Fecha y hora</span>
-			  						<img src="http://placehold.it/195x50" />
-			  						<a href="btn btn-small btn-primary">Enviame un recordatorio</a>
-			  					</li>
-			  					<li>
-			  						<span class="label label-info">Fecha y hora</span>
-			  						<img src="http://placehold.it/195x50" />
-			  						<a href="btn btn-small btn-primary">Enviame un recordatorio</a>
-			  					</li>
-			  								  					<li>
-			  						<span class="label label-info">Fecha y hora</span>
-			  						<img src="http://placehold.it/195x50" />
-			  						<a href="btn btn-small btn-primary">Enviame un recordatorio</a>
-			  					</li>
-			  								  					<li>
-			  						<span class="label label-info">Fecha y hora</span>
-			  						<img src="http://placehold.it/195x50" />
-			  						<a href="btn btn-small btn-primary">Enviame un recordatorio</a>
-			  					</li>
+			  				<?php foreach ($events->entry as $entry) {
+      						$title = stripslashes($entry->title);
+      						$summary = stripslashes($entry->summary);
+      						$summary_pre=str_replace('á', '&aacute;', $summary);
+      						$summary_lol=explode('<br>', $summary_pre);
+      						$summary_final=$summary_lol[0];
+      						?>
+      							<li>
+      								<h5><?= $title; ?></h5>
+      								<span><?= $summary_final; ?></span><br />
+      								<a href="<?= $entry->link->attributes()->href; ?>" target="_blank">Guardar en mi calendario</a>
+      							</li>
+      						<?php }; ?>
 			  				</ul>
 			  				<object width="100%" height="600" id="obj_1297019226277"><param name="movie" value="http://chatsc2mx.chatango.com/group"><param name="wmode" value="transparent"><param name="AllowScriptAccess" value="always"><param name="AllowNetworking" value="all"><param name="AllowFullScreen" value="true"><param name="flashvars" value="cid=1297019226277&amp;a=CCCCCC&amp;b=51&amp;f=43&amp;i=87&amp;k=999999&amp;l=FFFFFF&amp;m=FFFFFF&amp;o=30&amp;r=100&amp;s=1"><embed id="emb_1297019226277" src="http://chatsc2mx.chatango.com/group" width="100%" height="600" wmode="transparent" allowscriptaccess="always" allownetworking="all" type="application/x-shockwave-flash" allowfullscreen="true" flashvars="cid=1297019226277&amp;a=CCCCCC&amp;b=51&amp;f=43&amp;i=87&amp;k=999999&amp;l=FFFFFF&amp;m=FFFFFF&amp;o=30&amp;r=100&amp;s=1"></object>
 			  				<h3>Patrocinios</h3>
